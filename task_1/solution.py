e113e0b216342ceeb790449d4ce29f1ed9c73946
Yevhenii <107956026+Geg3mon@users.noreply.github.com>
2022-10-09 22:15:59 +0300

    Update solution.py

--- task_1/solution.py
+++ task_1/solution.py
@@ -2,23 +2,23 @@ import json
 import os
 
 
-def get_json_from_csv(filename: str) -> dict[str, (dict[str, list]), (str, int)] :
+def get_json_from_csv(filename: str) -> dict[str, (dict[str, list]), (str, int)]:
     path = os.path.abspath(filename)
     with open(path, 'r') as f:
         data = f.read().split("\n")[1:]
     json_data: dict[str, (dict[str, list]), (str, int)] = {}
-    for person in data:
-        person = person.split(',')
+    for line in data:
+        line = line.split(',')
         # This check help with whitespace in file
-        if person[0] == '':
+        if line[0] == '' or line[1] == '':
             continue
         # Add person to existing country object
-        elif person[0] in json_data:
-            json_data[person[0]]['people'].append(person[1])
-            json_data[person[0]]['count'] = len(json_data[person[0]]['people'])
-        # Add first country with person
+        elif line[0] in json_data:
+            json_data[line[0]]['people'].append(line[1])
+            json_data[line[0]]['count'] += 1
+        # Add first country with line
         else:
-            json_data[person[0]] = {'people': [person[1]], 'count': 1}
+            json_data[line[0]] = {'people': [line[1]], 'count': 1}
 
     return json.dumps(json_data, indent=2)
 
